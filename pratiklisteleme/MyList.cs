feb033e380362e2a2e13bd9bce45170657cd105d
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-08-13 16:13:17 +0300

    Proje dosyası ekle.

---
+++ pratiklisteleme/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratiklisteleme
+{
+    public class MyList<T>
+    {
+        T[] _array;
+        T[] _tamparray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tamparray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tamparray.Length; i++)
+            {
+                _array[i] = _tamparray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return _array[index];
+            }
+
+        }
+    }
+}
---
+++ pratiklisteleme/Program.cs
@@ -0,0 +1,14 @@
+﻿using pratiklisteleme;
+
+MyList<string> davetliler = new MyList<string>();
+davetliler.Add("Hülya Koçyiğit");
+davetliler.Add("Zekeriya palabıyık");
+davetliler.Add("Hakan Altun");
+davetliler.Add("Zeki Müren");
+
+Console.WriteLine("!!! DAVETLİLER !!!");
+
+for  (int i = 0; i < davetliler.Count; i++)
+{
+    Console.WriteLine($"{i+1} - {davetliler[i]}");
+}
